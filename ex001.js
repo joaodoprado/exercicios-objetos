b5e31c316b5a1fc5048e33412a7f9a8cdddacd8b
Joao <jv.prado98@gmail.com>
2023-07-28 15:00:37 -0300

    ex001

---
+++ ex001.js
@@ -0,0 +1,20 @@
+const endereco = {
+    rua: "Aqui",
+    numero: "1",
+    complemento: "La Ravardiere",
+    bairro: "Calhau",
+    cep: "65064-463",
+    cidade: "São Luis",
+    estado: "Maranhão",
+    pais: "Brasil"
+}
+
+const usuario = {
+    nome: "João",
+    email: "jv.prado98@gmail.com",
+    telefone: "(98)98414-7530",
+    data_nasc: "04/08/1998",
+    endereco : endereco
+}
+
+console.log(usuario)
